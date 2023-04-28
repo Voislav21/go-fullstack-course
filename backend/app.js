64cfe836670d10697e83c61d2dbef18ebc0e5b14
Voislav Ivanovski <voislav21@gmail.com>
2023-04-28 17:36:28 +0200

    first api/stuff communicating with frontend

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Request received!');
+app.use((reg,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Your request was successful!' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Response sent successfully!');
+app.use('/api/stuff', (reg,res,next) => {
+  const stuff = [
+    {
+      _id: 'holoouugjfk',
+      title: 'My first thing',
+      description:'All info about my first thing',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Gitarre_und_Schattenspiel_01.jpg',
+      price: 4900,
+      userId: 'randompop',
+    },
+    {
+      _id: 'mlqdsfhjo',
+      title: 'My second thing',
+      description:'All info about my second thing',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Gitarre_und_Schattenspiel_01.jpg',
+      price: 100099,
+      userId: 'randompop2',
+    },
+  ];
+  res.status(200).json(stuff);
 });
 
 module.exports = app;
