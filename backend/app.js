16ba61bbb7e017d1c16f18fbae22aadd9c098ed9
Voislav Ivanovski <voislav21@gmail.com>
2023-05-02 14:35:22 +0200

    routes added

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff')
 
 const app = express();
 
@@ -29,94 +29,6 @@ app.use((reg,res,next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (reg,res,next) => {
-  const thing = new Thing({
-    title: reg.body.title,
-    description: reg.body.description,
-    imageUrl: reg.body.imageUrl,
-    price: reg.body.price,
-    userId: reg.body.userId
-  });
-  thing.save()
-    .then(() => {
-      res.status(201).json({
-        message: 'Post saved successfully!'
-      });
-    }).catch((error) => {
-      res.status(400).json({
-        error: error
-      });
-    });
-});
-
-app.get('/api/stuff/:id', (reg,res,next) => {
-  Thing.findOne({
-    _id: reg.params.id
-  }).then(
-    (thing) => {
-      res.status(200).json(thing);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-app.put('/api/stuff/:id', (reg,res,next)=>{
-  const thing = new Thing({
-    _id: reg.params.id,
-    title: reg.body.title,
-    description: reg.body.description,
-    imageUrl: reg.body.imageUrl,
-    price: reg.body.price,
-    userId: reg.body.userId
-  });
-  Thing.updateOne({_id: reg.params.id}, thing).then(
-    () => {
-      res.status(201).json({
-        message: 'Thing updated successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-app.delete('/api/stuff/:id', (reg,res,next) => {
-  Thing.deleteOne({_id: reg.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-app.use('/api/stuff', (reg,res,next) => {
-  Thing.find().then(
-    (things) => {
-      res.status(200).json(things);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
+app.use('/api/stuff', stuffRoutes)
 
 module.exports = app;
